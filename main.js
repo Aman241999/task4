66d65c51d71c2048751882f91d7d1682a0e6dccf
Aman241999 <138651751+Aman241999@users.noreply.github.com>
2023-07-07 17:14:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
+function saveTolocalStorage(event){
+    event.preventDefault();
+    let name=event.target.Username.value;
+    let email=event.target.emailId.value;
+    let phonenumber=event.target.phonenumber.value;
+    let dob=event.target.dob.value;
+    localStorage.setItem("name",name);
+    localStorage.setItem("email",email);
+    localStorage.setItem("phonenumber",phonenumber); 
+    localStorage.setItem("dob",dob);
 
-localStorage.setItem('name','Aman')
-console.log(localStorage.getItem('name'));
-//sessionStrage
-sessionStorage.setItem('name','nilesh');
-sessionStorage.setItem('name','rahul');//override the value
-console.log(sessionStorage.getItem('name'));
-//sessionStorage.removeItem('name');
-//cookies
-document.cookie='name=ramesh expires='+new Date(2020,0,1).toUTCString()
+
+
+}
